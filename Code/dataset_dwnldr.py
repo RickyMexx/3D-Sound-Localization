af5fc00e2f540e9a7708ead721e4820cb544aac6
RickyMexx <riccardo.caprari@gmail.com>
2020-05-02 13:35:10 +0200

    update

---
+++ Code/dataset_dwnldr.py
@@ -0,0 +1,3 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='1Lfiip7IC8YOOHyUcGTJeyBnRvHlgQay3', dest_path='../dataset.zip', unzip=True)
